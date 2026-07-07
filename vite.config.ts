3a3b83df6248bd67132c636a9735d27ecde71cf7
abrahamdrechsler <99840233+abrahamdrechsler@users.noreply.github.com>
2026-07-07 11:10:25 -0400

    Fix GitHub Pages routing

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  base: './',
+  base: '/replit-horizontal-beam-prototype/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
